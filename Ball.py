6e2a05d2791d226ab6d9aefb2aac15ec83f7a172
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2023-02-13 16:10:48 +0200

    gravity

---
+++ Ball.py
@@ -0,0 +1,40 @@
+import pygame as pg
+from pygame import Surface
+
+
+class Ball:
+    def __init__(self,
+                 x: int, y: int,
+                 velocity_x: int = 0, velocity_y: int = 0,
+                 acceleration_x: int = 0, acceleration_y: int = 0):
+        self.x = x
+        self.y = y
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
+        self.acceleration_x = acceleration_x
+        self.acceleration_y = acceleration_y
+
+    def atomic_move(self):
+        self.x += self.velocity_x
+        self.velocity_x += self.acceleration_x
+        self.y += self.velocity_y
+        self.velocity_y += self.acceleration_y
+
+    def gravity_up(self):
+        self.acceleration_x = 0
+        self.acceleration_y = -1
+
+    def gravity_down(self):
+        self.acceleration_x = 0
+        self.acceleration_y = 1
+
+    def gravity_left(self):
+        self.acceleration_x = -1
+        self.acceleration_y = 0
+
+    def gravity_right(self):
+        self.acceleration_x = 1
+        self.acceleration_y = 0
+
+    def draw(self, screen: Surface):
+        pg.draw.circle(screen, (0, 0, 255), (self.x, self.y), 3)
--- Visualization.py
+++ Visualization.py
@@ -1,29 +1,37 @@
 import pygame as pg
 
+from Ball import Ball
+
 
 class Visualization:
     def __init__(self):
-        self.screen = pg.display.set_mode([500, 500])
-        self.position_x = 250
-        self.position_y = 250
+        self.screen = pg.display.set_mode([16 * 60, 9 * 60])
+        self.ball = Ball(250, 250, acceleration_y=1)
 
     def update(self) -> bool:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return True
             elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    print(1)
+                if event.key == pg.K_UP or event.key == pg.K_w:
+                    self.ball.gravity_up()
+                if event.key == pg.K_DOWN or event.key == pg.K_s:
+                    self.ball.gravity_down()
+                if event.key == pg.K_LEFT or event.key == pg.K_a:
+                    self.ball.gravity_left()
+                if event.key == pg.K_RIGHT or event.key == pg.K_d:
+                    self.ball.gravity_right()
+
+        self.ball.atomic_move()
 
         self.screen.fill((255, 255, 255))
-        self.position_x -= 1
-        pg.draw.circle(self.screen, (0, 0, 255), (self.position_x, self.position_y), 75, width=4)
+        self.ball.draw(self.screen)
 
-    def run(self):
+    def run(self) -> None:
         pg.init()
         while True:
-            pg.time.delay(30)
+            pg.time.delay(100)
             if self.update():
                 pg.quit()
-                break
+                return
             pg.display.update()
