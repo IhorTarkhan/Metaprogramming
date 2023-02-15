bd15fd913e31960a20b18582549414e453bd37a3
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2023-02-15 12:54:30 +0200

    rework with pymunk

--- Engine.py
+++ Engine.py
@@ -1,66 +1,87 @@
-import pygame as pg
-from pygame.event import Event
-from sympy import Point
+import math
+import random
 
-from state.Direction import Direction
-from state.MainState import MainState
-from visualization.MainVisualization import MainVisualization
+import pygame
+import pymunk
+from pygame import Vector2 as VectorPG, Color
+from pygame.event import Event
+from pymunk import Vec2d as VectorPM
+
+g = 98.1
 
 
 class Engine:
     def __init__(self):
-        self.state = MainState()
-        self.visualization = MainVisualization(self.state)
+        self.screen = pygame.display.set_mode((16 * 60, 9 * 60))
+        self.clock = pygame.time.Clock()
+
+        self.space = pymunk.Space()
+        self.space.gravity = VectorPM(0.0, -g)
+
+        self.ball = self.create_ball()
+        self.walls = self.create_walls()
+
+    def to_pm(self, point: VectorPG) -> VectorPM:
+        return VectorPM(point.x, self.screen.get_height() - point.y)
+
+    def to_pg(self, point: VectorPM) -> VectorPG:
+        return VectorPG(point.x, self.screen.get_height() - point.y)
+
+    def create_ball(self) -> pymunk.Shape:
+        center = VectorPG(self.screen.get_width() / 2, self.screen.get_height() / 2)
+        rand_r = min(center) * 0.7 * random.random()
+        rand_angle = 2 * math.pi * random.random()
+        position = center + VectorPG(rand_r * math.sin(rand_angle), rand_r * math.cos(rand_angle))
+
+        body = pymunk.Body(1, 1)
+        body.position = self.to_pm(position)
+        shape = pymunk.Circle(body, 10)
+        shape.elasticity = 1
+        self.space.add(body, shape)
+        return shape
+
+    def create_walls(self) -> list[pymunk.Segment]:
+        walls = []
+
+        center = VectorPG(self.screen.get_width() / 2, self.screen.get_height() / 2)
+        r = min(center) * 0.9
+        n = 30
+        min_angle = 2 * math.pi / n
+        for i in range(n - 2):
+            p1 = VectorPG(r * math.sin(min_angle * i), r * math.cos(min_angle * i)) + center
+            p2 = VectorPG(r * math.sin(min_angle * (i + 1)), r * math.cos(min_angle * (i + 1))) + center
+            shape = pymunk.Segment(self.space.static_body, self.to_pm(p1), self.to_pm(p2), 0.0)
+            shape.elasticity = 1
+            self.space.add(shape)
+            walls.append(shape)
+        return walls
 
     def run(self) -> None:
-        pg.init()
+        pygame.init()
         while True:
-            events = pg.event.get()
-            if any(e.type == pg.QUIT for e in events):
+            events = pygame.event.get()
+            if any(e.type == pygame.QUIT for e in events):
                 break
             self.handle_frame(events)
-            self.visualization.visualize()
-            pg.display.update()
-            pg.time.delay(300)
-        pg.quit()
+            pygame.display.set_caption("FPS: " + str(int(self.clock.get_fps() * 10) / 10))
+            pygame.display.update()
+            self.space.step(1 / 60)
+            self.clock.tick(60)
+        pygame.quit()
 
     def handle_frame(self, events: list[Event]):
         for event in events:
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_UP or event.key == pg.K_w:
-                    self.state.direction = Direction.UP
-                elif event.key == pg.K_DOWN or event.key == pg.K_s:
-                    self.state.direction = Direction.DOWN
-                elif event.key == pg.K_LEFT or event.key == pg.K_a:
-                    self.state.direction = Direction.LEFT
-                elif event.key == pg.K_RIGHT or event.key == pg.K_d:
-                    self.state.direction = Direction.RIGHT
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    self.space.gravity = VectorPM(0, g)
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    self.space.gravity = VectorPM(0, -g)
+                elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    self.space.gravity = VectorPM(-g, 0)
+                elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    self.space.gravity = VectorPM(g, 0)
 
-        predict_position, predict_velocity = self.free_fall_prediction()
-        distance = Point(predict_position).distance(self.state.screen.center)
-        print(distance)
-        self.state.ball.x = predict_position[0]
-        self.state.ball.y = predict_position[1]
-        self.state.ball.velocity_x = predict_velocity[0]
-        self.state.ball.velocity_y = predict_velocity[1]
-
-    def free_fall_prediction(self):
-        ball = self.state.ball
-        direction = self.state.direction
-
-        new_x = ball.x + ball.velocity_x
-        new_y = ball.y + ball.velocity_y
-
-        if direction == Direction.UP:
-            new_velocity_x = ball.velocity_x
-            new_velocity_y = ball.velocity_y - 1
-        elif direction == Direction.DOWN:
-            new_velocity_x = ball.velocity_x
-            new_velocity_y = ball.velocity_y + 1
-        elif direction == Direction.LEFT:
-            new_velocity_x = ball.velocity_x - 1
-            new_velocity_y = ball.velocity_y
-        else:
-            new_velocity_x = ball.velocity_x + 1
-            new_velocity_y = ball.velocity_y
-        return (new_x, new_y), (new_velocity_x, new_velocity_y)
+        self.screen.fill(Color("WHITE"))
+        pygame.draw.circle(self.screen, Color("BLUE"), self.to_pg(self.ball.body.position), self.ball.radius)
+        for w in self.walls:
+            pygame.draw.line(self.screen, Color("BLACK"), self.to_pg(w.a), self.to_pg(w.b))
--- state/ScreenState.py
+++
@@ -1,6 +0,0 @@
-class ScreenState:
-    def __init__(self):
-        self.width = 16 * 60
-        self.height = 9 * 60
-        self.center = (self.width / 2, self.height / 2)
-
